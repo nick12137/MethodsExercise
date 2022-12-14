c8210bbe72d784ec6fcb81faa52e9e6d89cda8af
Nikolay Loboda <nick12137@gmail.com>
2022-12-14 00:08:56 -0600

    Began questions for user

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, what is your name?"); //output
+
+            var name = Console.ReadLine(); // input
+
+            Console.WriteLine("What is your favorite color?"); //output
+
+            var color = Console.ReadLine(); // input
+
+            Console.WriteLine("What is your favorite car?"); //output
+
+            var car = Console.ReadLine(); // input
+
+            Console.WriteLine("Do you like dogs or cats?"); //output
+
+            var animals = Console.ReadLine(); // input
+
+            Console.WriteLine("What kind of music do you like?"); //output
+
+            var music = Console.ReadLine(); // input
         }
     }
+
+
+
+
 }
+//Name: Michael
+
+//Favorite Color: Blue
+
+//Favorite Animal: Walrus
+
+//Favorite Band: The Beatles
+
