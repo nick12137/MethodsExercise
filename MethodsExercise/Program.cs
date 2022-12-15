1102422d7aab5c734e0c210a5a18f26f0fffc6cd
Nikolay Loboda <nick12137@gmail.com>
2022-12-14 23:36:14 -0600

    Finished profile configuration

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -8,21 +8,26 @@
 
             var userName = Console.ReadLine(); // input
 
-            Console.WriteLine("What is your favorite color?"); //output
+            Console.WriteLine($"Nice to meet you {userName}. What is your favorite color?"); //output
 
             var color = Console.ReadLine(); // input
 
-            Console.WriteLine("What is your favorite car?"); //output
+            Console.WriteLine($"{color} is an awesome color. Do you have a favorite car, and what is it?"); //output
 
             var car = Console.ReadLine(); // input
 
-            Console.WriteLine("What is your favorite animal?"); //output
+            Console.WriteLine("Awesome, what about your favorite animal?"); //output
 
             var animal = Console.ReadLine(); // input
 
             Console.WriteLine("What kind of music do you like?"); //output
 
             var music = Console.ReadLine(); // input
+
+            Console.WriteLine($"Thanks, {userName}! Here is your profile."); //output
+            Console.WriteLine("------------------------------------------");
+         
+          
 
             Console.WriteLine($"Name: {userName}");
             Console.WriteLine($"Favorte color: {color}");
